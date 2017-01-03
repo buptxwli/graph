34789efa3df8a821d65b9f696826c66f38387c3d
xwli <buptlorraine@163.com>
2017-01-03 21:38:34 +0800

    the input of simulation including big crossing and small crossing

--- input_graph.py
+++ input_graph.py
@@ -2,11 +2,10 @@ row=int(input('row:'))
 col=int(input('col:'))
 type=int(input('type:'))
 out=[[0 for cols in range(col)]for rows in range(row)]
-# out=[[0]*len(col)]*len(row)
 def graph1(row,col):  #small crossing
 	for rows in range(row):
 		for cols in range (col):
-			if rows==0 :
+			if rows==0 or rows==row-1:
 				out[rows][cols]='#'
 			if 1<=rows and rows<row/2 and cols%2!=0:
 				out[rows][cols]='>'
@@ -16,10 +15,21 @@ def graph1(row,col):  #small crossing
 				out[rows][cols]='X'
 			if (rows%4==2 and rows!=row-1 and cols%4==2)or(rows%4==3 and rows!=row-1 and cols%2==0)or(rows%4==0 and cols%4==0 and rows!=0):
 				out[rows][cols]='Y'
-			if rows==row-1 :
+def graph2(row,col): #big crossing
+	for rows in range(row):
+		for cols in range(col):
+			if rows==0 or rows==row-1:
 				out[rows][cols]='#'
+			if 1<=rows and rows<row/2 and cols%2!=0:
+				out[rows][cols]='>'
+			if rows<=row-2 and rows>=row/2 and cols%2!=0:
+				out[rows][cols]='<'
+			if rows!=0 and rows!=row-1 and cols%2==0:
+				out[rows][cols]='X'
 if type==1:
 	graph1(row,col)
+else:
+	graph2(row,col)
 f = open('output.txt', 'w+')
 for line in out:
     for c in line:
